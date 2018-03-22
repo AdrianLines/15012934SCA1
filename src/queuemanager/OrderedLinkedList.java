f717b4467201d3c4bbaacd486e30aa34d6221db8
15012934 <15012934@sc4171.uhi.ad.local>
2018-03-22 13:03:56 +0000

    SL,UL toString fixed, comments added to SL and UA.

--- src/queuemanager/OrderedLinkedList.java
+++ src/queuemanager/OrderedLinkedList.java
@@ -13,7 +13,7 @@ public class OrderedLinkedList<T> implements PriorityQueue<T> {
      LinkList theLinkedList = new LinkList();
         public int priority;
         public T item;
-        public String itemString;
+       
 
         public OrderedLinkedList next;
         
@@ -28,29 +28,27 @@ public class OrderedLinkedList<T> implements PriorityQueue<T> {
          
     }
     
-//        public void display(){
-//            System.out.println(item + ": " + priority);
-//            
-//        }
+
         
-        public String toString(){
-            String itemString = String.valueOf(item);
-          return itemString;  
+        public String toString(){ //takes the object "item" and puts it to string
+            String result = theLinkedList.theString(); //sets theLinkedList through theString method to get the string for it
+           
+          return result;  //returns the string
         }
 
     @Override
-    public void add(T item, int priority) throws QueueOverflowException {
+    public void add(T item, int priority) throws QueueOverflowException { //creates a new link using the item and priority variables
         
    
         
-        theLinkedList.insertFirstLink(item,priority);
-               theLinkedList.display();
+        theLinkedList.insertFirstLink(item,priority); //calls the "insertFirstLink" method 
+            
        
         
     }
 
     @Override
-    public T head() throws QueueUnderflowException {
+    public T head() throws QueueUnderflowException { //returns the highest priority item, in the case of the ordered list it should always be the first item.
      
         if (!theLinkedList.isEmpty()){
     
@@ -65,15 +63,15 @@ public class OrderedLinkedList<T> implements PriorityQueue<T> {
     public void remove() throws QueueUnderflowException { //removes the item with the highest priority
         if (!theLinkedList.isEmpty()){
         theLinkedList.removeFirst(); //runs the removeLink method
-        theLinkedList.display(); //outputs the current list because the "P" output doesn't work
+        
         }else{
              throw new QueueUnderflowException();
         }
     }
 
     @Override
-    public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean isEmpty() { //checks if the list is empty.
+        return theLinkedList.isEmpty(); //basically just checks if the firstLink in the list is null or not.
     }
         
         
@@ -82,16 +80,21 @@ public class OrderedLinkedList<T> implements PriorityQueue<T> {
         
 class LinkList{
     public OrderedLinkedList firstLink;
-     public Object PriorityItem = null;
-    LinkList(){
+     public Object PriorityItem = null; //creates and object for the highest prio item, used in the remove method
+    LinkList(){ //sets the list to be empty
         firstLink = null;
         
     }
     
-    public boolean isEmpty(){
+    
+    /*Checks if the list is empty*/
+    public boolean isEmpty(){ //checks if the list is empty by checking the first link
         return(firstLink == null);
     }
     
+    
+    
+    /*Inserts first Link*/
     public void insertFirstLink(T item, int priority){
          OrderedLinkedList pointerLink = firstLink; //creates a new link that is used to scroll through the list
          OrderedLinkedList newLink = new OrderedLinkedList(item, priority); //creates a new link that is set to be the newLink
@@ -106,33 +109,35 @@ class LinkList{
       
                  
                  if(pointerLink == firstLink){
-                   //  System.out.println("FIRST LINK ADDED");
+                        //adds link in the first position
                      
                      newLink.next = firstLink;
                      firstLink = newLink;
                  }else{
+                     //adds link into anywhere that isn't the first link
                      prevLink.next = newLink;
                     newLink.next = pointerLink;
-                // System.out.println("link added break");
+
                  }
                     
                     break; // finish the loop
                     
                 }else if (pointerLink.next == null){
+                    //adds link to the end of the list
                     pointerLink.next = newLink;
-                //     System.out.println("link added");
+
              
                     break;
                     
                     
-                }else{prevLink = pointerLink;
+                }else{prevLink = pointerLink; // scrolls to the next link
                     pointerLink = pointerLink.next;
                 }
                 
                
                 
         }
-                if(firstLink==null){
+                if(firstLink==null){ //if the list is empty it creates a firstLink
                     System.out.println("firstlink added");
                 
                     firstLink = newLink;
@@ -141,12 +146,12 @@ class LinkList{
         
     } 
         
-   
+   /* Removes the first Link from the list*/
     public void removeFirst(){
-        OrderedLinkedList linkReference = firstLink;
+        
                 if(!isEmpty()){
                     System.out.println("link removed");
-        firstLink = firstLink.next;
+        firstLink = firstLink.next; //sets the second link to be the first link, removing the first link.
     }else{
                     System.out.println("Empty List");
                 }
@@ -154,9 +159,33 @@ class LinkList{
                
     }
     
-     
     
-    public void display(){
+    /*Returns the string value for the list*/
+     public String theString(){ //displays the contents of the linked list
+        OrderedLinkedList theLink = firstLink;
+        String result = "[";
+        Object item;
+     
+        String priority;
+        while(theLink != null){         //while loop that runs through the list outputting each item
+            if(theLink != firstLink){
+              result = result + ", ";  
+            }
+            priority = Integer.toString(theLink.priority); //sets priority value
+            item =   (Object)theLink.item; //sets the item value
+            
+            
+            result = result +item + ", " + priority; //adds the values to the string with a comma in between them
+            
+            theLink = theLink.next;
+            
+        }
+        result = result +"]"; //closes the string with a bracket
+        return result;
+    }
+    
+     /*Method used during testing to help display the list*/
+    public void display(){ //DISPLAY METHOD ENTIRELY USED FOR TESTING
         OrderedLinkedList theLink = firstLink;
         while(theLink != null){
             System.out.println("this Link: " + theLink +" "+theLink.priority);
@@ -167,11 +196,11 @@ class LinkList{
     }
     
     
-    
+    /*Finds the highest priority item in the list, in this case it's always the first item*/
     public T find(T item, int priority){
         
         PriorityItem = new Object();
-        //Object PriorityItem = newPriorityItem<>(item, priority);
+ 
         
         
         
@@ -179,26 +208,12 @@ class LinkList{
        item = (T) firstLink.item;
         priority = firstLink.priority;
         
-       PriorityItem =  new PriorityItem<>(item, priority); //the PriorityItem is created from the highLink 
+       PriorityItem =  new PriorityItem<>(item, priority); //the PriorityItem is created from the firstLink
         
         return ((PriorityItem<T>) PriorityItem).getItem(); //returns the item from priorityItem
     }
     
     
-    public int highest(){
-        int highest = 0;
-                OrderedLinkedList theLink = firstLink;
-        
-        while(theLink != null){
-           
-            if(theLink.priority > highest){
-                highest = theLink.priority;
-            }
-            theLink = theLink.next;
-            
-        }
-        return highest;
-        
-    }
+    
   }
 }
